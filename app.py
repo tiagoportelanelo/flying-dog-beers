516e8044fd89a295d4fdb34b75c7751800f1234b
tiagoportelanelo <53179437+tiagoportelanelo@users.noreply.github.com>
2019-07-22 14:10:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table as dt
 import plotly.graph_objs as go
 # Load datasets
 train = pd.read_csv('train.csv', index_col ='PassengerId' )
